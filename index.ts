d01473f2b93cb3d86ae031f8b2f9f644ad46029d
Benjamin Gudehus <hastebrot@gmail.com>
2018-02-15 20:09:55 +0100

    (refactor) Introduce main block, and extract functions.

--- index.ts
+++ index.ts
@@ -1,69 +1,89 @@
 import got from "got"
 import { queue } from "d3-queue"
 
-const config = require("./build/config.json")
+if (require.main === module) {
+  const config = require("./build/config.json")
+  // fetchProjectList(config)
+  fetchMergeRequestList(config)
+}
 
-const run = (action: () => void) => action()
+export function pathsProjects(): string {
+  return "/v4/projects?per_page=100"
+}
 
-const request = (path: string) => got(config.gitlab.api + path, {
-  headers: {
-    "Private-Token": config.gitlab.token,
-    "User-Agent": "github.com/hastebrot/gitlab-summary"
-  }
-})
+export function pathsMergeRequests(projectId: string): string {
+  return "/v4/projects/" + projectId + "/merge_requests"
+    + "?state=opened&per_page=100"
+}
 
-// run(async () => {
-//   const path = "/v4/projects?per_page=100"
-//   const response = await request(path)
-//   console.log(response.statusCode, response.statusMessage)
+export function pathsMergeRequest(projectId: string,
+                                  mergeRequestId: string): string {
+  return "/v4/projects/" + projectId + "/merge_requests/"
+    + mergeRequestId + "/changes?per_page=100"
+}
 
-//   const projects: any[] = JSON.parse(response.body)
-//   projects.forEach(project => {
-//     console.log(project.id, project.name, project.path_with_namespace, project._links.merge_requests)
-//   })
-// })
+export function fetchProjectList(config: any) {
+  run(async () => {
+    const path = pathsProjects()
+    const response = await request(config, path)
+    console.log(response.statusCode, response.statusMessage)
 
-run(async () => {
-  const path = "/v4/projects/" + config.gitlab.project + "/merge_requests?state=opened&per_page=100"
-  const response = await request(path)
-  console.log(response.statusCode, response.statusMessage)
-
-  const mergeRequests: any[] = JSON.parse(response.body)
-  const ids: number[] = []
-  mergeRequests.forEach(mergeRequest => {
-    // console.log(mergeRequest.id, mergeRequest.iid, mergeRequest.title,
-    //   mergeRequest.author.username, mergeRequest.work_in_progress)
-    ids.push(mergeRequest.iid)
-  })
-
-  const q = queue()
-  const results: any = []
-  ids.forEach(id => {
-    q.defer(async done => {
-      const path = "/v4/projects/" + config.gitlab.project + "/merge_requests/" + id + "/changes?per_page=100"
-      const response = await request(path)
-      console.log(response.statusCode, response.statusMessage)
-
-      const mergeRequest: any = JSON.parse(response.body)
-      results.push({
-        id: mergeRequest.iid,
-        title: mergeRequest.title,
-        author: mergeRequest.author.username,
-        files: mergeRequest.changes.map((it: any) => it.new_path).length,
-        update: mergeRequest.updated_at
-      })
-      done(null)
+    const projects: any[] = JSON.parse(response.body)
+    projects.forEach(project => {
+      console.log(project.id, project.name, project.path_with_namespace, project._links.merge_requests)
     })
   })
+}
 
-  q.await(() => {
-    console.log(results)
+export function fetchMergeRequestList(config: any) {
+  run(async () => {
+    const path = pathsMergeRequests(config.gitlab.project)
+    const response = await request(config, path)
+    console.log(response.statusCode, response.statusMessage)
+
+    const mergeRequests: any[] = JSON.parse(response.body)
+    const ids: string[] = []
+    mergeRequests.forEach(mergeRequest => {
+      // console.log(mergeRequest.id, mergeRequest.iid, mergeRequest.title,
+      //   mergeRequest.author.username, mergeRequest.work_in_progress)
+      ids.push(mergeRequest.iid)
+    })
+
+    const q = queue()
+    const results: any = []
+    ids.forEach(id => {
+      q.defer(async done => {
+        const path = pathsMergeRequest(config.gitlab.project, id)
+        const response = await request(config, path)
+        console.log(response.statusCode, response.statusMessage)
+
+        const mergeRequest: any = JSON.parse(response.body)
+        results.push({
+          id: mergeRequest.iid,
+          title: mergeRequest.title,
+          author: mergeRequest.author.username,
+          files: mergeRequest.changes.map((it: any) => it.new_path).length,
+          update: mergeRequest.updated_at
+        })
+        done(null)
+      })
+    })
+
+    q.await(() => {
+      console.log(results)
+    })
   })
-})
+}
 
-// http://gitlab.example.com/users/auth/gitlab/callback
-// curl https://gitlab.example.com/api/v4/projects?private_token=1a2b3c4d5e
-// curl --header "Private-Token: 1a2b3c4d5e" https://gitlab.example.com/api/v4/projects
+function run(action: () => void) {
+  action()
+}
 
-// https://docs.gitlab.com/ee/api/projects.html#list-all-projects
-// https://docs.gitlab.com/ee/api/merge_requests.html#list-project-merge-requests
+function request(config: any, path: string) {
+  return got(config.gitlab.api + path, {
+    headers: {
+      "Private-Token": config.gitlab.token,
+      "User-Agent": "github.com/hastebrot/gitlab-summary"
+    }
+  })
+}
